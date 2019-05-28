d68adc1472580aec5c0cad1279400ae3e6a6db00
Fabrice Coulon <fabrice.coulon74@gmail.com>
2019-05-28 09:25:05 +0200

    Wip build a simple button-based navigation

---
+++ src/components/MainTabs.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="main-tabs">
+    <!-- A simple menu made of one active button and inactive or
+    muted-buttons. This is the way with w-bind or simply ':class'
+    to conditionnally apply a css class style based on some object props.
+    -->
+    <span v-for="t in maintabs" :key="t.id">
+      <button :class="{'button': t.selected, 'muted-button': !t.selected}">{{ t.name }}</button>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main-tabs',
+  props: {
+    maintabs: Array
+  }
+}
+</script>
+
+<style scoped>
+button {
+  margin-top: 1rem;
+  margin-right: 1rem;
+}
+</style>
---
+++ src/components/Owners.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>Owners</h1>
+      <!-- Retrieving events from the child component OwnerForm
+      and reroute it to this application addOwner. '@' is the shortcut
+      for Vue's directive 'v-on' -->
+      <owner-form @add:owner="addOwner"></owner-form>
+      <!-- Binding the component attribute owners to this array of owners
+      ':' is the shortcut for Vue's directive 'v-bind'
+      -->
+      <owners-table :owners="owners"></owners-table>
+  </div>
+</template>
+
+<script>
+import OwnersTable from '@/components/OwnersTable.vue'
+import OwnerForm from '@/components/OwnerForm.vue'
+
+export default {
+  name: 'owners',
+  components: {
+    OwnersTable,
+    OwnerForm
+  },
+  data() {
+    return {
+      owners: [
+        { id:1, fname: "fname1", lname: "lname1", email: "fname1.lname1@email.com" },
+        { id:2, fname: "fname2", lname: "lname2", email: "fname2.lname2@email.com" }
+      ]
+    }
+  },
+  methods: {
+    addOwner(owner) {
+      const nbOwners = this.owners.length;
+      const lastId = (nbOwners > 0) ? this.owners[nbOwners-1].id : 0;
+      const id = lastId + 1;
+      const newOwner = { ...owner, id };
+      this.owners = [ ...this.owners, newOwner ];
+    }
+  },
+  mounted() {
+    // This method is called once after Vue has been 'mounted' to a given DOM
+    // which is specified in main.js with function mount()
+    //alert("Vue has been mounted on #app")
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
